ab6feb187df397d2ce41e6fdbe3e3e34c759cf74
LoicZHU <zhu.loic@gmail.com>
2024-07-14 23:49:17 +0200

    feat: add streaming mode
    Co-authored-by: zakaryalahmadi <80518856+Zakaryalahmadi@users.noreply.github.com>
    Co-authored-by: Mazene-ZERGUINE <92375034+Mazene-ZERGUINE@users.noreply.github.com>

--- src/main/scala/progfun/Main.scala
+++ src/main/scala/progfun/Main.scala
@@ -1,10 +1,37 @@
 package fr.esgi.al.funprog
 
-import progfun.services.MowerController
+import scala.util._
+
+import progfun.config._
+import progfun.services._
 
 @main
 def Main(): Unit = {
   println("🚀 Program is running!")
+  val config = AppConfig.load()
 
-  MowerController.executeMowers()
+  println("Select mode (1 = streaming, 2 = full): ")
+  val mode = scala.io.StdIn.readInt()
+  mode match {
+    case 1 => {
+      println("📡 Streaming mode selected")
+      MowerController.executeMowersStreaming(config)
+    }
+    case 2 => {
+      val fileContent = FileService.readFile(
+        s"${System.getProperty("user.dir")}${config.inputPath}"
+      )
+      fileContent match {
+        case Failure(ex) => {
+          println(s"💩 An error occurred: ${ex.getMessage}")
+        }
+        case Success(lines) => {
+          MowerController.executeMowers(config, lines)
+        }
+      }
+    }
+    case _ => {
+      println("💩 Invalid mode selected")
+    }
+  }
 }
--- src/main/scala/progfun/services/MowerController.scala
+++ src/main/scala/progfun/services/MowerController.scala
@@ -1,83 +1,70 @@
 package progfun.services
 
+import scala.annotation.tailrec
 import scala.util._
 
-import progfun.config.AppConfig
+import progfun.config._
 import progfun.models._
 import progfun.utils._
 import upickle.default._
 
 object MowerController {
 
-  def executeMowers(): Unit = {
-    val config = AppConfig.load()
+  def executeMowers(config: AppConfig, lines: List[String]): Unit = {
     val absoluteProjectPath = System.getProperty("user.dir")
-    val fileContent =
-      FileService.readFile(s"${absoluteProjectPath}${config.inputPath}")
 
-    fileContent match {
-      case Failure(ex) => {
-        println(s"💩 An error occurred: ${ex.getMessage}")
-      }
-      case Success(lines) => {
-        println("🚜 Starting the mowers...")
+    lines.headOption match {
+      case None => println("💩 The input file is empty or incorrect.")
+      case Some(lawnLine) =>
+        val lawn = parseLawn(lawnLine)
+        val mowers = FileService.parseMowers(lines.drop(1))
 
-        lines.headOption match {
-          case None => println("💩 The input file is empty or incorrect.")
-          case Some(lawnLine) =>
-            val lawn = parseLawn(lawnLine)
-            val mowers = FileService.parseMowers(lines.drop(1))
+        val finalMowers = mowers.map { mower =>
+          InstructionService.move(mower, lawn)
+        }
 
-            val finalMowers = mowers.map { mower =>
-              InstructionService.move(mower, lawn)
-            }
-
-            val mowerOutputs =
-              mowers.zip(finalMowers).map { case (initialMower, finalMower) =>
-                val initialPosition = PositionOutput(
-                  Point(
-                    initialMower.position.point.x,
-                    initialMower.position.point.y
-                  ),
-                  Direction.toChar(initialMower.position.direction)
-                )
-                val finalPosition = PositionOutput(
-                  Point(
-                    finalMower.position.point.x,
-                    finalMower.position.point.y
-                  ),
-                  Direction.toChar(finalMower.position.direction)
-                )
-
-                MowerOutput(
-                  debut = initialPosition,
-                  instructions =
-                    initialMower.instructions.map(Instruction.toChar),
-                  fin = finalPosition
-                )
-              }
-
-            val lawnOutput = LawnOutput(
-              Point(lawn.topRightBound.x, lawn.topRightBound.y),
-              mowerOutputs
+        val mowerOutputs =
+          mowers.zip(finalMowers).map { case (initialMower, finalMower) =>
+            val initialPosition = PositionOutput(
+              Point(
+                initialMower.position.point.x,
+                initialMower.position.point.y
+              ),
+              Direction.toChar(initialMower.position.direction)
+            )
+            val finalPosition = PositionOutput(
+              Point(
+                finalMower.position.point.x,
+                finalMower.position.point.y
+              ),
+              Direction.toChar(finalMower.position.direction)
             )
 
-            val jsonString = write(lawnOutput, indent = 2)
-            val jsonPath = s"${absoluteProjectPath}${config.jsonPath}"
-            this.generateJsonFile(jsonString, jsonPath)
+            MowerOutput(
+              debut = initialPosition,
+              instructions = initialMower.instructions.map(Instruction.toChar),
+              fin = finalPosition
+            )
+          }
 
-            val csvString = CsvUtils.toCsv(lawnOutput)
-            val csvPath = s"${absoluteProjectPath}${config.csvPath}"
-            this.generateCsvFile(csvString, csvPath)
+        val lawnOutput = LawnOutput(
+          Point(lawn.topRightBound.x, lawn.topRightBound.y),
+          mowerOutputs
+        )
 
-            val yamlString = YamlUtils.toYaml(lawnOutput)
-            val yamlPath = s"${absoluteProjectPath}${config.yamlPath}"
-            this.generateYamlFile(yamlString, yamlPath)
+        val jsonString = write(lawnOutput, indent = 2)
+        val jsonPath = s"${absoluteProjectPath}${config.jsonPath}"
+        this.generateJsonFile(jsonString, jsonPath)
 
-            this.printFinalPosition(finalMowers)
-        }
-      }
+        val csvString = CsvUtils.toCsv(lawnOutput)
+        val csvPath = s"${absoluteProjectPath}${config.csvPath}"
+        this.generateCsvFile(csvString, csvPath)
 
+        val yamlString = YamlUtils.toYaml(lawnOutput)
+        val yamlPath = s"${absoluteProjectPath}${config.yamlPath}"
+        this.generateYamlFile(yamlString, yamlPath)
+
+        this.printFinalPosition(finalMowers)
     }
   }
 
@@ -126,6 +113,143 @@ object MowerController {
 
       println(s"📍 Final position: ${x} ${y} ${orientation}")
     }
+  }
+
+  def executeMowersStreaming(config: AppConfig): Unit = {
+    val absoluteProjectPath = System.getProperty("user.dir")
+    val bufferedSource = scala.io.Source.stdin.getLines()
+
+    println("Please enter the lawn dimensions (e.g. '5 5'):")
+    val lawnLine = bufferedSource.next()
+    val lawn = parseLawn(lawnLine)
+    val mowers = readMowers(bufferedSource, List.empty)
+
+    this.generateOutputs(config, absoluteProjectPath, lawn, mowers.reverse)
+  }
+
+  @tailrec
+  private def readMowers(
+      bufferedSource: Iterator[String],
+      acc: List[Mower]): List[Mower] = {
+    println("Please enter the mower initial position (e.g., '1 2 N'):")
+    if (!bufferedSource.hasNext) acc
+    else {
+      val positionLine = bufferedSource.next()
+      if (positionLine.isEmpty) acc
+      else {
+        println("Please enter the instructions (e.g., 'GAGAGAGAA'):")
+        if (!bufferedSource.hasNext) acc
+        else {
+          val instructionLine = bufferedSource.next()
+          if (instructionLine.isEmpty) acc
+          else {
+            val mowerOption = for {
+              x               <- positionLine.split(" ").headOption.map(_.toInt)
+              y               <- positionLine.split(" ").lift(1).map(_.toInt)
+              orientationChar <- positionLine.split(" ").lift(2).map(_.head)
+              orientation <- Direction.fromChar(orientationChar) match {
+                case Some(orientation) => Some(orientation)
+                case None              => Some(South)
+              }
+            } yield Mower(
+              Position(Point(x, y), orientation),
+              instructionLine.flatMap(Instruction.fromChar).toList
+            )
+
+            val newAcc = mowerOption match {
+              case Some(mower) => mower :: acc
+              case None        => acc
+            }
+            readMowers(bufferedSource, newAcc)
+          }
+        }
+      }
+    }
+  }
+
+//  private def readMowers(bufferedSource: Iterator[String]): List[Mower] = {
+//    val mowersBuffer = ListBuffer.empty[Mower]
+//
+//    while (bufferedSource.hasNext) {
+//      println("Please enter the mower initial position (e.g., '1 2 N'):")
+//      val positionLine = bufferedSource.next()
+//      if (positionLine.isEmpty) return mowersBuffer.toList
+//
+//      println("Please enter the instructions (e.g., 'GAGAGAGAA'):")
+//      val instructionLine = bufferedSource.next()
+//      if (instructionLine.isEmpty) return mowersBuffer.toList
+//
+//      val mowerOption = for {
+//        x               <- positionLine.split(" ").headOption.map(_.toInt)
+//        y               <- positionLine.split(" ").lift(1).map(_.toInt)
+//        orientationChar <- positionLine.split(" ").lift(2).map(_.head)
+//        orientation <- Direction.fromChar(orientationChar) match {
+//          case Some(orientation) => Some(orientation)
+//          case None              => Some(South)
+//        }
+//      } yield Mower(
+//        Position(Point(x, y), orientation),
+//        instructionLine.flatMap(Instruction.fromChar).toList
+//      )
+//
+//      mowerOption.foreach(mowersBuffer += _)
+//    }
+//
+//    mowersBuffer.toList
+//  }
+
+  private def generateOutputs(
+      config: AppConfig,
+      absoluteProjectPath: String,
+      lawn: Lawn,
+      mowers: List[Mower]
+  ): Unit = {
+    val finalMowers = mowers.map { mower =>
+      InstructionService.move(mower, lawn)
+    }
+
+    val mowerOutputs =
+      mowers.zip(finalMowers).map { case (initialMower, finalMower) =>
+        val initialPosition = PositionOutput(
+          Point(
+            initialMower.position.point.x,
+            initialMower.position.point.y
+          ),
+          Direction.toChar(initialMower.position.direction)
+        )
+        val finalPosition = PositionOutput(
+          Point(
+            finalMower.position.point.x,
+            finalMower.position.point.y
+          ),
+          Direction.toChar(finalMower.position.direction)
+        )
+
+        MowerOutput(
+          debut = initialPosition,
+          instructions = initialMower.instructions.map(Instruction.toChar),
+          fin = finalPosition
+        )
+      }
+
+    val lawnOutput = LawnOutput(
+      Point(lawn.topRightBound.x, lawn.topRightBound.y),
+      mowerOutputs
+    )
+
+    val jsonString = write(lawnOutput, indent = 2)
+    val jsonPath = s"${absoluteProjectPath}${config.jsonPath}"
+    this.generateJsonFile(jsonString, jsonPath)
+
+    val csvString = CsvUtils.toCsv(lawnOutput)
+    val csvPath = s"${absoluteProjectPath}${config.csvPath}"
+    this.generateCsvFile(csvString, csvPath)
+
+    val yamlString = YamlUtils.toYaml(lawnOutput)
+    val yamlPath = s"${absoluteProjectPath}${config.yamlPath}"
+    this.generateYamlFile(yamlString, yamlPath)
+
+    this.printFinalPosition(finalMowers)
   }
 
 }
