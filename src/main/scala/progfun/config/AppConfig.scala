fc4ad20634894f522d9f23b478a03efb2d091681
LoicZHU <zhu.loic@gmail.com>
2024-07-21 18:48:12 +0200

    feat: update mode with config
    Co-authored-by: zakaryalahmadi <80518856+Zakaryalahmadi@users.noreply.github.com>
    Co-authored-by: Mazene-ZERGUINE <92375034+Mazene-ZERGUINE@users.noreply.github.com>

--- src/main/scala/progfun/config/AppConfig.scala
+++ src/main/scala/progfun/config/AppConfig.scala
@@ -2,10 +2,12 @@ package progfun.config
 
 import scala.util._
 
+import progfun.models._
 import progfun.utils.ConfigUtils
 
 final case class AppConfig(
     name: String,
+    mode: String,
     csvPath: String,
     inputPath: String,
     jsonPath: String,
@@ -17,18 +19,21 @@ object AppConfig {
 
   def load(): AppConfig = {
     ConfigUtils.loadConfig("src/main/resources/config.json") match {
-      case Failure(ex) =>
+      case Failure(ex) => {
         println(s"💩 Failed to load config: ${ex.getMessage}")
         sys.exit(1)
-      case Success(config) =>
+      }
+      case Success(config) => {
         AppConfig(
           config.getOrElse("name", "funprog"),
+          config.getOrElse("mode", Mode.FULL),
           config.getOrElse("csvPath", "/tmp/output.csv"),
           config.getOrElse("inputPath", "/tmp/input.txt"),
           config.getOrElse("jsonPath", "/tmp/output.json"),
           config.getOrElse("logPath", "/tmp/output.log"),
           config.getOrElse("yamlPath", "/tmp/output.yaml")
         )
+      }
     }
   }
 
